3a1cdccf42c11cceb599f5c81aff12f7461ef9e3
alex <c.alex.pellett@gmail.com>
2018-12-20 21:00:32 -0500

    doc

--- R/acresFSA-package.R
+++ R/acresFSA-package.R
@@ -1,2 +1,8 @@
-#' acresFSA
+#' United States Farm Service Agency Acreage Data
 #'
+#' Some datasets from the United States Energy Information Administration.
+#'
+#' @docType package
+#' @name acresFSA
+#'
+NULL
