757cca5473161c36e5b85d3ca61b03b466e75ded
Alex Pellett <PellettC@dnr.sc.gov>
2023-12-29 11:10:44 -0500

    start documentation

---
+++ R/data-acresFSA.r
@@ -0,0 +1,16 @@
+#' @title Annual crop acreage by county and irrigation
+#' @description A table compiled from FSA reports from 2009 to 2021
+#' @format A \code{data.frame} with 9 variables
+#' \describe{
+#' \item{Year}{}
+#' \item{State}{state name in all caps}
+#' \item{County}{county name in all caps}
+#' \item{Type}{}
+#' \item{Irrigation Practice}{}
+#' \item{Crop}{}
+#' \item{Variety}{}
+#' \item{Intended Use}{}
+#' \item{Acres}{}
+#' }
+"acresFSA"
+
